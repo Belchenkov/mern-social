d1b8a98bb1cff81e9768e65d3b4a086854280451
Aleksey Belchenkov <u608110@gmail.com>
2020-05-06 19:24:18 +0300

    Adding APIs to follow and unfollow

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -19,6 +19,11 @@ router.route('/api/users/:userId')
     .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
     .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove);
 
+router.route('/api/users/follow')
+    .put(authCtrl.requireSignin, userCtrl.addFollowing, userCtrl.addFollower);
+router.route('/api/users/unfollow')
+    .put(authCtrl.requireSignin, userCtrl.removeFollowing, userCtrl.removeFollower)
+
 router.param('userId', userCtrl.userByID);
 
 export default router;
