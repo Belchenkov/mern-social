c43933a942288f4c6b28f90a3d85a9a0b2d51267
Aleksey Belchenkov <u608110@gmail.com>
2020-05-12 21:20:13 +0300

    Get listi posts from API

---
+++ server/controllers/post.controller.js
@@ -0,0 +1,27 @@
+import formidable from 'formidable';
+import fs from 'fs';
+
+import Post from '../models/post';
+import errorHandler from './../helpers/dbErrorHandler';
+
+const listNewsFeed = async (req, res) => {
+    let following = req.profile.following;
+    following.push(req.profile._id);
+
+    try {
+        let posts = await Post.find({
+            postedBy: {
+                $in : req.profile.following
+            }})
+            .populate('comments.postedBy', '_id name')
+            .populate('postedBy', '_id name')
+            .sort('-created')
+            .exec();
+
+        res.json(posts);
+    } catch(err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        });
+    }
+}
